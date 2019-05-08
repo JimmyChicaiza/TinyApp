9e6185e448e68ae33a7636a166323db54ab26b19
JimmyChicaiza <jimmy.chicaiza@gmail.com>
2019-05-08 19:40:24 +0000

    moving foreward

--- express_server.js
+++ express_server.js
@@ -39,15 +39,21 @@ app.get("/urls", (req, res) => { // maybe remove json form "/urls.json"
     res.render("urls_show", templateVars);
 
   });
-  
+
+  app.post("/urls/:shortURL/delete", (req, res) => {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  });
+
+
 //   app.get("/urls", (req, res) => {
 //     let templateVars = { urls: urlDatabase };
 //     res.render("urls_index", templateVars);
 //   });
-            app.get("/u/:shortURL", (req, res) => {
-                const longURL = urlDatabase[req.params.shortURL];
-             res.redirect(longURL);
-            });
+app.get("/u/:shortURL", (req, res) => {
+const longURL = urlDatabase[req.params.shortURL];
+res.redirect(longURL);
+ });
 
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
