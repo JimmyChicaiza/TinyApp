db112578fe0288c7466dda12775ce97152e42d7c
JimmyChicaiza <jimmy.chicaiza@gmail.com>
2019-05-09 21:38:03 +0000

    moving on with cookies

--- express_server.js
+++ express_server.js
@@ -19,7 +19,7 @@ var urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//ALL MY GETS - EVERYTHING THAT THE USER WILL SEE DISPLAY ON THE WEB BROWSER.
+//ALL MY GETs - EVERYTHING THAT THE USER WILL SEE DISPLAY ON THE WEB BROWSER.
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -49,6 +49,7 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -65,34 +66,34 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/urls/:shortURL/show", (req, res) => {  //**********
-  const keyname = generateRandomString();
+app.post("/urls/:shortURL", (req, res) => {  //**********
+  const shortURL = req.params.shortURL;
   const toReplaceId = req.body.newURL; //I need to create a variable that replace urlDatabase.
-  urlDatabase[keyname] = toReplaceId;
+  urlDatabase[shortURL] = toReplaceId;
 
-  console.log({ urlDatabase }); //to console.log ({}) to see the full object.
+  //console.log({ urlDatabase }); //to console.log ({}) to see the full object.
   // var urlDatabase = {
   //     "b2xVn2": "http://www.lighthouselabs.ca",
   //     "9sm5xK": "http://www.google.com",
   //     }
 
-  res.render("urls_show");
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
-  const longUrl = req.body.longURL;
+const longUrl = req.body.longURL;
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.post("/urls/:id", (req, res) => {
   //POST to update the URL resource
-  console.log(req.body);
-  res.send("Ok");
+console.log(req.body);
+res.send("Ok");
 });
 
 // ALL MY LISTEN TO SEE IF THE PROGRAM WORKS.
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+console.log(`Example app listening on port ${PORT}!`);
 });
